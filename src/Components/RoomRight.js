e45c12f7cd443fbf0f71ab00e301c022f94018a5
EternalMarch <32583303+EternalMarch@users.noreply.github.com>
2021-11-21 00:11:30 +0100

    Room updates

--- src/Components/RoomRight.js
+++ src/Components/RoomRight.js
@@ -6,28 +6,40 @@ const RoomRight = (props) => {
     
     return (
         <div id="room-right-section">
-            <div id="room-UI">
+            
 
                 <div id="player-UI">
+                    <div id="playlist-preview">
+                        <img src="https://lite-images-i.scdn.co/image/ab67616d0000b273f130779b779893bf7334c66d"></img>
+                        <h2>Playlist name</h2>
+                        <p>This is the short description of the playlist!</p>
+                    </div>
+                    <div id="playlist-controller">
+                        <button>Previous</button>
+                        <button>Play</button>
+                        <button>Pause</button>
+                        <button>Next</button>
 
+                        <input type="text" placeholder="Your guess"></input>
+                        <input type="text" placeholder="Send message"></input>
+                    </div>
                 </div>
 
 
 
 
-                <div id="players-container">
-                <p id="players-in-room-title">Players in room</p>
-                    <hr></hr>
-                    <div id="players-list">
-                        <div class="player-card">
-                            <p>EternalMarch</p>
-                        </div>
-                        <div class="player-card">
-                            <p>Riccina</p>
-                        </div>
-                        <div class="player-card">
-                            <p>Felle21</p>
-                        </div>
+            <div id="players-container">
+            <p id="players-in-room-title">Players in room</p>
+                <hr></hr>
+                <div id="players-list">
+                    <div class="player-card">
+                        <p>User1</p>
+                    </div>
+                    <div class="player-card">
+                        <p>Guest</p>
+                    </div>
+                    <div class="player-card">
+                        <p>Felle21</p>
                     </div>
                 </div>
             </div>
